be66799b05f839c7df2f710d36409d2bf8dd486b
simonovtony <simonovtony@outlook.com>
2016-07-01 18:18:17 +0300

    18:18 01.07.2016

---
+++ json/5-http/example4.php
@@ -0,0 +1,27 @@
+<?php
+
+header("Access-Control-Allow-Origin: *");
+header("Content-type: application/json; charset=UTF-8");
+
+$conn = new mysqli("myServer", "myUser", "myPassword", "Northwind");
+
+$result = $conn->query("SELECT CompanyName, City, Country FROM Customers");
+
+$outp = "[";
+
+while($rs = $result->fetch_array(MYSQLI_ASSOC)) {
+    if($outp != "[") {
+        $outp .= ",";
+    }
+    $outp .= '{"Name" : ' . $rs["CompanyName"] . '",';
+    $outp .= '"City" : ' . $rs["CompanyName"] . '",';
+    $outp .= '"Country" : ' . $rs["CompanyName"] . '"}';
+}
+
+$outp .= "]";
+
+$conn->close();
+
+echo ($outp);
+
+?>
