d26b708dd5e696edd5af00505e5ec394a31722f5
Anton Simonov <simonovtony@outlook.com>
2016-09-24 18:22:36 +0300

    18:22 24.09.2016

--- php-basic/part-5/chapter-31/t_autoload.php
+++
@@ -1,6 +0,0 @@
-<?php ## Проверка __autoload().
-function __autoload($cls) {
-  echo "Запрос на загрузку класса $cls<br>";
-}
-echo NonExisted::$a;
-?>
--- php-basic/part-5/chapter-31/t_testlib.php
+++
@@ -1,7 +0,0 @@
-<?php ## Пример использования библиотеки.
-require_once "lib/config.php";
-require_once "TestLib.php";
-# ...
-echo TestLib::length("abcd")."<br>";
-echo TestLib::poweredLength("abcd");
-?>
