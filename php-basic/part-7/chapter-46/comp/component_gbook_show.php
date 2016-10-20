04396964e09e5700d8063414a955c572f9bf881f
simonovtony <simonovtony@outlook.com>
2016-10-20 11:45:03 +0300

    11:44 20.10.2016

---
+++ php-basic/part-7/chapter-46/comp/component_gbook_show.php
@@ -0,0 +1,8 @@
+<?php
+
+define("GBook1", "gbook.dat");
+require_once "model.php";
+
+$Data = LoadBook(GBook1);
+
+?>
---
+++ php-basic/part-7/chapter-46/smarty/tmp/%%59^59F^59F43357%%header.tpl.php
@@ -0,0 +1,8 @@
+<?php /* Smarty version 2.6.30, created on 2016-10-20 10:43:10
+         compiled from inc/header.tpl */ ?>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Smarty</title>
+    </head>
+    <body>
